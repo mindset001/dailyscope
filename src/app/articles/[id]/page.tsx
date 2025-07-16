6c27f954f166cf7f603368509d5dcb73e7490d9b
mindset001 <shittumoshoodademola@gmail.com>
2025-07-16 17:16:32 +0100

    'dad'

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,51 +1,63 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { useParams, useSearchParams } from 'next/navigation';
-import ArticlesSection from '../components/Article';
-import One from '@/../public/images/article2.png';
+// app/articles/[id]/page.js
+import React from 'react';
+import Image from 'next/image';
+import { articles } from '../data/articles';
+import ArticlesSection from '../components/Suggestions';
+
+// interface ArticleDetailProps {
+//   params: {
+//     id: string;
+//   };
+// }
 
 export default function ArticleDetail() {
-  const { id } = useParams();
-  const searchParams = useSearchParams();
-  const [article, setArticle] = useState<any>(null);
-
-  useEffect(() => {
-    const fromList = searchParams.get('fromList');
-    if (fromList === 'true') {
-      const data = sessionStorage.getItem('selectedArticle');
-      if (data) {
-        setArticle(JSON.parse(data));
-      }
-    }
-  }, [searchParams]);
-
-  if (!article) return <div className="text-center py-20">Loading article...</div>;
+  // const article = articles.find(article => article.id.toString() === params.id);
+  
+  // if (!article) {
+  //   return <div>Article not found</div>;
+  // }
 
   return (
     <div className="max-w-6xl mx-auto py-12 px-4">
       <div className="mb-8">
-        <h1 className="text-[55px] text-black font-[800] mt-4 mb-6 text-center">
-          {article.title} with {article.author}
-        </h1>
-        <img
-          src={One.src}
-          alt={article.title}
+        <h1 className="text-[55px] font-[800] mt-4 mb-6 text-center">{articles[0].title} with {articles[0].author}</h1>
+        <Image
+          src={articles[0].Image}
+          alt={articles[0].title}
           className="rounded-lg w-full"
         />
       </div>
+      
+      
+      <p className="text-lg text-gray-700 mb-8 w-[70%]">{articles[0].history}</p>
+      <div className='pr-10 my-8'>
+        <h2 className='text-[42px] font-[600]'>{articles[0].journey?.title}</h2>
+        <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].journey?.content}</p>
+      </div>
 
-      <p className="text-lg text-gray-700 mb-8 w-[70%]">{article.content}</p>
+      <div className='pr-10 my-8'>
+        <h2 className='text-[42px] font-[600]'>{articles[0].goals?.title}</h2>
+        <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].goals?.content}</p>
+      </div>
 
-      {article.journey && (
-        <div className="pr-10 my-8">
-          <h2 className="text-[42px] font-[600]">{article.journey.title}</h2>
-          <p className="text-justify font-[400] mt-4 text-[18px]">{article.journey.content}</p>
-        </div>
-      )}
+      <div className='pr-10 my-8'>
+        <h2 className='text-[42px] font-[600]'>{articles[0].process?.title}</h2>
+        <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].journey?.content}</p>
+      </div>
 
-      {/* Repeat for goals, process, projects etc. */}
+      <div className='pr-10 my-8'>
+        <h2 className='text-[42px] font-[600]'>{articles[0].process?.title}</h2>
+        <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].journey?.content}</p>
+      </div>
 
-      <ArticlesSection />
+      <div className='pr-10 my-8'>
+        <h2 className='text-[42px] font-[600]'>{articles[0].projects?.title}</h2>
+        <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].projects?.items[0].description}</p>
+                <p className='text-justify font-[400] mt-4 text-[18px]'>{articles[0].projects?.items[0].details}</p>
+      </div>
+      <div>
+        <ArticlesSection/>
+      </div>
     </div>
   );
 }
