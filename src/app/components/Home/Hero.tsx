4b82a38a0b0f1565cd5946fb293e9a03a371ba92
mindset001 <shittumoshoodademola@gmail.com>
2025-08-24 07:53:59 +0100

    'hero'

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
      
       
       {/* Hero content */}
-      <div className=" mx-auto h-full flex flex-col justify-center items-center text-black">
+      <div className=" mx-auto h-full flex flex-col justify-center items-center text-white">
         <h1 className="text-[40px] md:text-[100px] font-[600] mb-4 text-center md:leading-20">Culture. Art. <br />Commentary.</h1>
         <p className="px-4 md:px-0 text-[14px] md:text-[18px] font-[400] md:text-2xl mb-8 max-w-2xl text-center">Where creative minds converge to explore the intersection of art, culture, and contemporary discourse. Start Reading</p>
         <div className="flex flex-col md:flex-row  gap-4">
