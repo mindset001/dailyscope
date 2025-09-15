7b4e721bd7a52276afcce00931791b6d7733b527
mindset001 <shittumoshoodademola@gmail.com>
2025-09-16 00:29:12 +0100

    'free-spotlight'

--- src/app/spotlight/page.tsx
+++ src/app/spotlight/page.tsx
@@ -6,14 +6,13 @@ import ProtectedRoute from '@/lib/ProtectedRoute'
 
 function Page() {
   return (
-    <ProtectedRoute requireSubscription subscriptionRedirect="/dashboard/subscription">
+
       <main className='flex flex-col gap-10'>
         <div className='bg-[#f9f9f9] px-8 md:px-20 py-4 md:py-10 flex flex-col gap-16'>
           <Hero/>
           <Spotlight/>
         </div>
       </main>
-    </ProtectedRoute>
   )
 }
 
